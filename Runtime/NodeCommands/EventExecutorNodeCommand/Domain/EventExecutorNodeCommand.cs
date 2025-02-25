25932fa4254e2c100de6175194b936bed7160dde
Lalo <laurencioberro@hotmail.com>
2025-02-24 21:35:36 -0300

    [A] Added EventExecutorNodeCommand

---
+++ Runtime/NodeCommands/EventExecutorNodeCommand/Domain/EventExecutorNodeCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommandQueues.Core;
+using MVVM.Core;
+
+namespace ScenesLoaderSystem
+{
+    public class EventExecutorNodeCommand : NodeCommand, IDisposable
+    {
+        private readonly IEventViewModel _onSceneLoadStartedEventViewModel;
+        private readonly IEventViewModel _onSceneLoadDoneEventViewModel;
+
+        public EventExecutorNodeCommand(IEventViewModel onSceneLoadStartedEventViewModel, IEventViewModel onSceneLoadDoneEventViewModel)
+        {
+            _onSceneLoadStartedEventViewModel = onSceneLoadStartedEventViewModel;
+            _onSceneLoadDoneEventViewModel = onSceneLoadDoneEventViewModel;
+            _onSceneLoadDoneEventViewModel.OnEventRaised += NotifyDoneExecution;
+        }
+
+        public override void Execute()
+        {
+            _onSceneLoadStartedEventViewModel.RaiseEvent();
+        }
+
+        public void Dispose()
+        {
+            _onSceneLoadDoneEventViewModel.OnEventRaised -= NotifyDoneExecution;
+        }
+    }
+}
---
+++ Runtime/NodeCommands/EventExecutorNodeCommand/Installers/EventExecutorNodeCommandInstaller.cs
@@ -0,0 +1,17 @@
+﻿using CommandQueues.Core;
+using MVVM.Core;
+using UnityEngine;
+
+namespace ScenesLoaderSystem
+{
+    public class EventExecutorNodeCommandInstaller : NodeCommandInstaller
+    {
+        [SerializeField] private EventViewModelSO _onSceneLoadStartedEventViewModel;
+        [SerializeField] private EventViewModelSO _onSceneLoadDoneEventViewModel;
+        
+        protected override INodeCommand GetData()
+        {
+            return new EventExecutorNodeCommand(_onSceneLoadStartedEventViewModel.GetEventViewModel(), _onSceneLoadDoneEventViewModel.GetEventViewModel());
+        }
+    }
+}
